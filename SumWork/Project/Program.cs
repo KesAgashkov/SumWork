301921e197b5b8c301c21936aae017e911e2005e
Kostya <kostilio911@gmail.ru>
2022-11-10 17:50:03 +0400

    create First Array

--- SumWork/Project/Program.cs
+++ SumWork/Project/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] CreateFirstArray ()
+{
+    System.Console.WriteLine("Введите количество элементов в массиве");
+    int size = int.Parse (Console.ReadLine());
+    string [] firstArray =  new string [size];
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.WriteLine($"Введите {i+1} элемент массива и нажмите Enter");
+        firstArray[i] =(Console.ReadLine());
+    }
+    return firstArray;
+}
+string [] firstArr = CreateFirstArray();
+
